3aa2dde1f3a98d274d7b58d05f91f2a0d6a9ea60
RafaelBortolozoLuxSistemas <rafaelbortolozo@luxsistemas.com.br>
2023-01-09 16:25:33 -0300

    Código melhor organizado e com função UPDATE

--- Program.cs
+++ Program.cs
@@ -39,6 +39,28 @@ public class Program
         //    }
         //}
 
+        
+
+        // Conexão com Dapper
+        using (var connection = new SqlConnection(connectionString))
+        {
+            UpdateCategory(connection);
+            ListCategories(connection);
+            // CreateCategory(connection);
+        }
+    }
+
+    static void ListCategories(SqlConnection connection)
+    {
+        var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+        foreach (var item in categories)
+        {
+            Console.WriteLine($"{item.Id} - {item.Title}");
+        }
+    }
+
+    static void CreateCategory(SqlConnection connection)
+    {
         var category = new Category();
         category.Id = Guid.NewGuid();
         category.Title = "Amazon AWS";
@@ -59,25 +81,26 @@ public class Program
                                     "@Description," +
                                     "@Featured)";
 
-        // Conexão com Dapper
-        using (var connection = new SqlConnection(connectionString))
+        var rows = connection.Execute(insertSqlCategory, new
         {
-            var rows = connection.Execute(insertSqlCategory, new { 
-                category.Id,
-                category.Title,
-                category.Url,
-                category.Summary,
-                category.Order,
-                category.Description,
-                category.Featured
-            });
-            Console.WriteLine($"{rows} linha(s) inserida(s)");
+            category.Id,
+            category.Title,
+            category.Url,
+            category.Summary,
+            category.Order,
+            category.Description,
+            category.Featured
+        });
+        Console.WriteLine($"{rows} linha(s) inserida(s)");
+    }
 
-            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-            foreach (var item in categories)
-            {
-                Console.WriteLine($"{item.Id} - {item.Title}");
-            }
-        }
+    static void UpdateCategory(SqlConnection connection)
+    {
+        var updateQuery = "UPDATE [Category] SET [Title]=@title WHERE [Id]=@id";
+        var rows = connection.Execute(updateQuery, new { 
+            id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
+            title = "Frontend 2023"
+        });
+        Console.WriteLine($"{rows} registro(s) atualizado(s)");
     }
 }
