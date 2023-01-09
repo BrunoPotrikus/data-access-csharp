ee3f9a556cae9ecadf689a6bd146c0714a86f877
RafaelBortolozoLuxSistemas <rafaelbortolozo@luxsistemas.com.br>
2023-01-09 16:02:43 -0300

    Adicionado INSERT com connection.Execute()

--- Program.cs
+++ Program.cs
@@ -39,13 +39,44 @@ public class Program
         //    }
         //}
 
+        var category = new Category();
+        category.Id = Guid.NewGuid();
+        category.Title = "Amazon AWS";
+        category.Url = "amazon";
+        category.Summary = "Categoria destinada a serviços do AWS";
+        category.Order = 8;
+        category.Description = "AWS Cloud";
+        category.Featured = false;
+
+        var insertSqlCategory = "INSERT INTO " +
+                                    "[Category]" +
+                                "VALUES(" +
+                                    "@Id," +
+                                    "@Title," +
+                                    "@Url," +
+                                    "@Summary," +
+                                    "@Order," +
+                                    "@Description," +
+                                    "@Featured)";
+
         // Conexão com Dapper
         using (var connection = new SqlConnection(connectionString))
         {
+            var rows = connection.Execute(insertSqlCategory, new { 
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+            });
+            Console.WriteLine($"{rows} linha(s) inserida(s)");
+
             var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-            foreach (var category in categories)
+            foreach (var item in categories)
             {
-                Console.WriteLine($"{category.Id} - {category.Title}");
+                Console.WriteLine($"{item.Id} - {item.Title}");
             }
         }
     }
